a1fbbddd56e14a0ee17d15b758be59777a6cdd16
Pyros009 <Ricardo.f.morgado@gmail.com>
2026-07-08 17:20:21 +0100

    targets version uniformization

--- updater.py
+++ updater.py
@@ -3,22 +3,38 @@ import json
 from config_manager import state, private
 from logger import logger
 
-program_current = state["program_version"]
-targets_current = state["targets_version"]
+def version_tuple(v):
+    return tuple(map(str, v.split(".")))
 
 def check_updates():
-    
+    updates = dict()    
     response = requests.get(private["github_version"])
     repo_p_version = response.json()["program_version"]
     repo_t_version = response.json()["targets_version"]
     
-    if repo_p_version > program_current:
-        logger.info("Program version outdated! Updating program...")
-        return True
+    if version_tuple(repo_p_version) > version_tuple(state["program_version"]):
+        logger.info("Versao do programa desactualizado! A actualizar programa...")
+        updates["program"]=repo_p_version
     
-    if repo_t_version > targets_current:
-        logger.info("Target version outdated! Updating targets...")
-        return True
+    logger.info((repo_t_version))
+    logger.info((state["targets_version"]))
+    
+    if version_tuple(repo_t_version) > version_tuple(state["targets_version"]):
+        logger.info("Targets desactualizado! A actualizar targets...")
+        updates["targets"]=repo_t_version
+        
+    if "program" in updates:
+        #program_update()
+        ...
+    
+    if "targets" in updates:
+        #targets_update()
+        ...
+    
+    print(updates)
+    
+    #    logger.info(f"Versao do programa actualizado! Estamos na versao {state["program_version"]}")
+    #    logger.info(f"Versao dos targets actualizado! Estamos na versao {state["targets_version"]}")
 
 
 if __name__ == "__main__":
